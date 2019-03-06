e10fa327e9c0638ed3ca9e4bbb8404afeef51409
Jacob Schoerner <jacob.schoerner@gmail.com>
2019-03-06 16:56:27 +0100

    Refactored threading

--- app/src/main/java/com/example/streck_app/HTTP.java
+++ app/src/main/java/com/example/streck_app/HTTP.java
@@ -11,13 +11,7 @@ import java.util.concurrent.CountDownLatch;
 public class HTTP {
 
     public static String doRequest(final String page, final String params) {
-        //Cheap hack to be able to return vals from inside the thread
-        final String[] response = new String[1];
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        Thread req_thread = new Thread(new Runnable() {
-            @Override
-                    public void run() {
+        String response = "";
         try
 
             {
@@ -40,24 +34,12 @@ public class HTTP {
 
                 InputStreamReader in = new InputStreamReader(con.getInputStream());
                 BufferedReader br = new BufferedReader(in);
-                response[0] = br.readLine();
-                System.out.println(response[0]);
-                latch.countDown(); // Release await() in the test thread.
+                response = br.readLine();
+                System.out.println(response);
 
                 /* Code stolen from StackOverflow and meagerly modified starts here */
             } catch(IOException e) {System.out.println(e);}
-
-        }});
-        req_thread.start();
-
-        try {
-            latch.await();
-        }
-        catch (InterruptedException e) {
-            System.out.println(e);
-        }
-        return response[0];
-
+        return response;
     }
 
 }
--- app/src/main/java/com/example/streck_app/Login.java
+++ app/src/main/java/com/example/streck_app/Login.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -30,16 +33,32 @@ public class Login extends AppCompatActivity {
 
         android.widget.EditText nameField = findViewById(R.id.username);
         String username = nameField.getText().toString();
-        String params = "apiType=getID&username=" + username;
-        int userID = Integer.parseInt(HTTP.doRequest("api.php", params));
+        final String params = "apiType=getID&username=" + username;
+        class Req_Run implements Runnable {
+            private volatile int uid;
+            @Override
+            public void run() {
+                uid = Integer.parseInt(HTTP.doRequest("api.php", params));
+            }
 
+            public int getUid() {
+                return uid;
+            }
+        }
+        Req_Run r = new Req_Run();
+        Thread req_thread = new Thread(r);
+        req_thread.start();
+        try {
+            req_thread.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        int userID = r.getUid();
         if (userID != -1) {
             Intent loginInfo = new Intent(Login.this, MainActivity.class);
             loginInfo.putExtra("username", username);
             loginInfo.putExtra("id", userID);
             startActivity(loginInfo);
-
         }
     }
-
 }
